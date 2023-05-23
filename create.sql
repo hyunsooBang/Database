0e483710617a6cbbed60e2c3bfd351c2c8376a08
youdame <gmlwjd2400@naver.com>
2023-05-23 17:08:09 +0900

    update create.sql

---
+++ create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE professor (
+  id varchar(3),
+  name varchar(10),
+  department varchar(20),
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE course (
+  id varchar(6),
+  title varchar(20),
+  department varchar(20),
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE student (
+  id varchar(7),
+  name varchar(10),
+  department varchar(20),
+  PRIMARY KEY (id)
+);
+CREATE TABLE rating (
+  rating_id int,
+  course_id varchar(6),
+  prof_id varchar(3),
+  student_id varchar(7),
+  point int,
+  contents varchar(100),
+  PRIMARY KEY (rating_id),
+  FOREIGN KEY (course_id) REFERENCES course (id),
+  FOREIGN KEY (prof_id) REFERENCES professor (id),
+  FOREIGN KEY (student_id) REFERENCES student (id)
+);
