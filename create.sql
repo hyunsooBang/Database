7d1cb0c58a372bc363add521712a95fe897c4a50
youdame <gmlwjd2400@naver.com>
2023-05-23 18:02:35 +0900

    fix error create.sql & insert.sql

--- create.sql
+++ create.sql
@@ -5,14 +5,14 @@ CREATE TABLE professor (
   PRIMARY KEY (id)
 );
 
-
 CREATE TABLE course (
   id varchar(6),
-  title varchar(20),
+  title varchar(30),
   department varchar(20),
-  PRIMARY KEY (id)
+  prof_id varchar(3),
+  PRIMARY KEY (id),
+  FOREIGN KEY (prof_id) REFERENCES professor(id)
 );
-
 
 CREATE TABLE student (
   id varchar(7),
@@ -22,14 +22,14 @@ CREATE TABLE student (
 );
 
 CREATE TABLE rating (
-  rating_id int,
+  rating_id int AUTO_INCREMENT,
   course_id varchar(6),
   prof_id varchar(3),
   student_id varchar(7),
-  point int,
+  point numeric(3,2),
   contents varchar(100),
   PRIMARY KEY (rating_id),
-  FOREIGN KEY (course_id) REFERENCES course (id),
-  FOREIGN KEY (prof_id) REFERENCES professor (id),
-  FOREIGN KEY (student_id) REFERENCES student (id)
+  FOREIGN KEY (course_id) REFERENCES course(id),
+  FOREIGN KEY (prof_id) REFERENCES professor(id),
+  FOREIGN KEY (student_id) REFERENCES student(id)
 );
