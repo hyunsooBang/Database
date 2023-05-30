2fd495d5a34713263be9c4ef21998ec034a17c47
hyunsooBang <90560533+hyunsooBang@users.noreply.github.com>
2023-05-30 15:54:25 +0900

    Delete User.java

--- User.java
+++
@@ -1,9 +0,0 @@
-public class User extends Person {
-	public User(String name, String number) {
-		super(name, number);
-	}
-
-	public User(String username, String number, String address) {
-		super(username, number, address);
-	}
-}
